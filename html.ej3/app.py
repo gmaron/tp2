a23b53d30e93bf9dc02c2af9640d4ce284b43836
gmaron <gastonmaron@gmail.com>
2017-08-06 17:48:24 -0300

    Initial commit

---
+++ html.ej3/app.py
@@ -0,0 +1,19 @@
+# Lugar para poder los imports de python
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+# app.route define la ruta donde se debe acceder
+@app.route('/')
+def hello_world():
+    return render_template('form.html')
+
+@app.route('/form')
+def action_form(nom=None):
+    nom = request.args["nombre"]
+    return render_template('response.html', name=nom)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=1111)
